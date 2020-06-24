63a142160ebb37adf61e637af8f225a9631cda35
fabio-gz <fabiogomez.silva@gmail.com>
2020-06-23 20:18:57 -0500

    [add]first exercises

---
+++ javascript_tutorial/second_larg.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let first = nums[0]
+    let second = nums[1]
+    
+    for (let i of nums) {
+        if (i > first) {
+            second = first
+            first = i
+        }
+        if (i > second && i < first) {
+            second = i
+        }
+    }
+    return second
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
